4ef6ef797ea6ffd8f7d9f488de27262158759e4e
juhyang <jh15501551@gmail.com>
2018-11-12 10:55:14 +0900

    빈 로그인페이지, 관리자메뉴페이지, 사용자페이지 만들어놓기

---
+++ app/src/main/java/com/example/juhyang/hospital/ManagerMenu.java
@@ -0,0 +1,13 @@
+package com.example.juhyang.hospital;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManagerMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_menu);
+    }
+}
---
+++ app/src/main/java/com/example/juhyang/hospital/PrimaryMenu.java
@@ -0,0 +1,13 @@
+package com.example.juhyang.hospital;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrimaryMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primary_menu);
+    }
+}
