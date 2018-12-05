b8f2663e63988567b754ebfad11a2db82ea51975
juhyang <jh15501551@gmail.com>
2018-12-06 07:00:14 +0900

    스피너 값에 따른 화면 전환

--- app/src/main/java/com/example/juhyang/hospital/MainActivity.java
+++ app/src/main/java/com/example/juhyang/hospital/MainActivity.java
@@ -26,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
         };
 
 
-
         ProgressBar progress = (ProgressBar) findViewById(R.id.progress);
         progress_percent = 0;
 
--- app/src/main/java/com/example/juhyang/hospital/login.java
+++ app/src/main/java/com/example/juhyang/hospital/login.java
@@ -20,15 +20,33 @@ public class login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         ImageView b = (ImageView) findViewById(R.id.button);
+
+
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(
-                        getApplicationContext(), // 현재 화면의 제어권자
-                        ManagerMenu.class); // 다음 넘어갈 클래스 지정
-                startActivity(intent); // 다음 화면으로 넘어간다
+
+                Spinner spinner = (Spinner)findViewById(R.id.spinner1);
+                final String text = spinner.getSelectedItem().toString();
+
+                if(text.equals("재고관리자")) {
+                    Intent intent = new Intent(
+                            getApplicationContext(), // 현재 화면의 제어권자
+                            ManagerMenu.class); // 다음 넘어갈 클래스 지정
+                    startActivity(intent); // 다음 화면으로 넘어간다
+                } else if(text.equals("치료부")) {
+                    Intent intent = new Intent(
+                            getApplicationContext(), // 현재 화면의 제어권자
+                            PrimaryMenu.class); // 다음 넘어갈 클래스 지정
+                    startActivity(intent); // 다음 화면으로 넘어간다
+                }
+
+
             }
         });
+
+
+
 
     }
 
