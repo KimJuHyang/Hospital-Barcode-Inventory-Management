d81c6af240ad2980bde066c94eac2d4d37bd286e
juhyang <jh15501551@gmail.com>
2018-12-06 03:47:02 +0900

    승인버튼 다음 액티비티 전환

--- app/src/main/java/com/example/juhyang/hospital/login.java
+++ app/src/main/java/com/example/juhyang/hospital/login.java
@@ -1,9 +1,11 @@
 package com.example.juhyang.hospital;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.ImageView;
 
 
 public class login extends AppCompatActivity {
@@ -12,6 +14,22 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ImageView b = (ImageView) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        ManagerMenu.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
+            }
+        });
+
     }
+
+
 }
+
+
 
