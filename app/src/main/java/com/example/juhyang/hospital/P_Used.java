b0d9eca6221feac598808ffd76bd27c38d0823b9
juhyang <jh15501551@gmail.com>
2018-12-16 05:16:18 +0900

    이미지뷰 src로 변경/오타수정

--- app/src/main/java/com/example/juhyang/hospital/P_Used.java
+++ app/src/main/java/com/example/juhyang/hospital/P_Used.java
@@ -1,7 +1,10 @@
 package com.example.juhyang.hospital;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class P_Used extends Activity {
 
@@ -9,6 +12,31 @@ public class P_Used extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_p__used);
+
+        //클릭시 다음화면 넘어가기
+        ImageView b = (ImageView) findViewById(R.id.add_used);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent change = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        P_camera.class); // 다음 넘어갈 클래스 지정
+                startActivity(change);
+            }
+        });
+
+        ImageView a = (ImageView) findViewById(R.id.searchSick);
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent change = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        P_sick_DB.class); // 다음 넘어갈 클래스 지정
+                startActivity(change);
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/juhyang/hospital/PrimaryMenu.java
+++ app/src/main/java/com/example/juhyang/hospital/PrimaryMenu.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -24,5 +25,44 @@ public class PrimaryMenu extends AppCompatActivity {
 
         final TextView myname = (TextView)findViewById(R.id.nameText);
         myname.setText(name); // 이름 표시
+
+        //클릭시 다음화면 넘어가기
+        ImageView a = (ImageView) findViewById(R.id.used);
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent change = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        P_Used.class); // 다음 넘어갈 클래스 지정
+                startActivity(change);
+            }
+        });
+
+        //클릭시 다음화면 넘어가기
+        ImageView b = (ImageView) findViewById(R.id.request);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent change = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        P_Request.class); // 다음 넘어갈 클래스 지정
+                startActivity(change);
+            }
+        });
+
+        //클릭시 다음화면 넘어가기
+        ImageView c = (ImageView) findViewById(R.id.mylist);
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent change = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                       P_my_request_list.class); // 다음 넘어갈 클래스 지정
+                startActivity(change);
+            }
+        });
     }
 }
