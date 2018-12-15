c4d1c1216a36080ab74b87146d86469ea7127ff5
juhyang <jh15501551@gmail.com>
2018-12-16 05:01:34 +0900

    재고요청,사용품입력 UI

---
+++ app/src/main/java/com/example/juhyang/hospital/P_sick_DB.java
@@ -0,0 +1,14 @@
+package com.example.juhyang.hospital;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class P_sick_DB extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p_sick__db);
+    }
+
+}
